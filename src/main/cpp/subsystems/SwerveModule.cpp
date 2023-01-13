a8e9be7048f92ca5f5574183ba3ddd81347452f0
=KadenKreisler <=kaden.kreisler@outlook.com>
2023-01-12 18:12:07 -0600

    added basic auto to working code

---
+++ src/main/cpp/subsystems/SwerveModule.cpp
@@ -0,0 +1,121 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/SwerveModule.h"
+
+#include <numbers>
+
+#include <frc/geometry/Rotation2d.h>
+
+#include "Constants.h"
+
+// SwerveModule::SwerveModule(int driveMotorChannel, int turningMotorChannel,
+//                            const int driveEncoderPorts[],
+//                            const int turningEncoderPorts[],
+//                            bool driveEncoderReversed,
+//                            bool turningEncoderReversed)
+//     : m_driveMotor(driveMotorChannel),
+//       m_turningMotor(turningMotorChannel),
+//       m_driveEncoder(driveEncoderPorts[0], driveEncoderPorts[1]),
+//       m_turningEncoder(turningEncoderPorts[0], turningEncoderPorts[1]),
+//       m_reverseDriveEncoder(driveEncoderReversed),
+//       m_reverseTurningEncoder(turningEncoderReversed) {
+  // Set the distance per pulse for the drive encoder. We can simply use the
+  // distance traveled for one rotation of the wheel divided by the encoder
+  // resolution.
+//   m_driveEncoder.SetDistancePerPulse(
+//       ModuleConstants::kDriveEncoderDistancePerPulse);
+
+//   // Set the distance (in this case, angle) per pulse for the turning encoder.
+//   // This is the the angle through an entire rotation (2 * std::numbers::pi)
+//   // divided by the encoder resolution.
+//   m_turningEncoder.SetDistancePerPulse(
+//       ModuleConstants::kTurningEncoderDistancePerPulse);
+
+//   // Limit the PID Controller's input range between -pi and pi and set the input
+//   // to be continuous.
+//   m_turningPIDController.EnableContinuousInput(
+//       units::radian_t{-std::numbers::pi}, units::radian_t{std::numbers::pi});
+// }
+SwerveModule::SwerveModule(int m_MotorController, rev::SparkMaxRelativeEncoder::Type m_EncoderType, int m_counts_per_rev, 
+int m_MotorControllerTurning, 
+ bool driveEncoderReversed,
+ int TurningEncoderNumber,
+ bool turningEncoderReversed
+)
+
+{
+         this->m_EncoderType = m_EncoderType;
+         this->m_counts_per_rev = m_counts_per_rev;
+         m_driveMotor = new rev::CANSparkMax(m_MotorController, rev::CANSparkMax::MotorType::kBrushless);
+         m_turningMotor = new rev::CANSparkMax(m_MotorControllerTurning, rev::CANSparkMax::MotorType::kBrushless);
+        //  samDriveEncoder = new rev::CANEncoder(*samDriveMotor, m_EncoderType, m_counts_per_rev);
+         m_driveEncoder = new rev::SparkMaxRelativeEncoder(m_turningMotor->GetEncoder(m_EncoderType, m_counts_per_rev));
+        //  samTurningEncoder = new rev::CANEncoder(*samTurningMotor, m_EncoderTypeTurning, m_counts_per_revTurning);
+         m_turningEncoder = new ctre::phoenix::sensors::CANCoder(TurningEncoderNumber);	
+
+         m_reverseDriveEncoder = driveEncoderReversed;
+         m_reverseTurningEncoder = turningEncoderReversed;
+         m_turningEncoder->ConfigSensorDirection(m_reverseTurningEncoder);
+        //Can't independently invert drive endcoder from drive motor.
+
+  // Set the distance per pulse for the drive encoder. We can simply use the
+  // distance traveled for one rotation of the wheel divided by the encoder
+  // resolution.
+  m_driveEncoder->SetPositionConversionFactor(
+      ModuleConstants::kDriveEncoderDistancePerPulse);
+
+  // Set the distance (in this case, angle, radians) per pulse for the turning encoder.
+  // This is the the angle through an entire rotation (2 * wpi::numbers::pi)
+  // divided by the encoder resolution.
+  m_turningEncoder->ConfigFeedbackCoefficient(
+  0.00153980788, "Radians", ctre::phoenix::sensors::SensorTimeBase());
+  // Limit the PID Controller's input range between -pi and pi and set the input
+  // to be continuous.
+  m_turningPIDController.EnableContinuousInput(units::radian_t(-std::numbers::pi),
+                                               units::radian_t(std::numbers::pi));
+  m_drivePIDController.SetP(
+    frc::SmartDashboard::PutNumber("Enter P Value" + std::to_string(m_driveMotor->GetDeviceId()),
+     ModuleConstants::kPModuleDriveController));
+  m_turningPIDController.SetP(
+    frc::SmartDashboard::PutNumber("Enter P Value for Turn" + std::to_string(m_turningMotor->GetDeviceId()), 
+    ModuleConstants::kPModuleTurningController));
+
+  frc::SmartDashboard::PutNumber("Wheel Offset " + std::to_string(m_turningMotor->GetDeviceId()), ModuleConstants::wheelOffset);
+
+}
+
+frc::SwerveModuleState SwerveModule::GetState() {
+  return {units::meters_per_second_t{m_driveEncoder->GetVelocity()},
+          units::radian_t{m_turningEncoder->GetPosition()}};
+}
+
+frc::SwerveModulePosition SwerveModule::GetPosition() {
+  return {units::meter_t{m_driveEncoder->GetPosition()},
+          units::radian_t{m_turningEncoder->GetPosition()}};
+}
+
+void SwerveModule::SetDesiredState(
+    const frc::SwerveModuleState& referenceState) {
+  // Optimize the reference state to avoid spinning further than 90 degrees
+  const auto state = frc::SwerveModuleState::Optimize(
+      referenceState, units::radian_t{m_turningEncoder->GetPosition()});
+
+  // Calculate the drive output from the drive PID controller.
+  const auto driveOutput = m_drivePIDController.Calculate(
+      m_driveEncoder->GetVelocity(), state.speed.value());
+
+  // Calculate the turning motor output from the turning PID controller.
+  auto turnOutput = m_turningPIDController.Calculate(
+      units::radian_t{m_turningEncoder->GetPosition()}, state.angle.Radians());
+
+  // Set the motor outputs.
+  m_driveMotor->Set(driveOutput);
+  m_turningMotor->Set(turnOutput);
+}
+
+void SwerveModule::ResetEncoders() {
+  // m_driveEncoder->Reset();
+  // m_turningEncoder->Reset();
+}
---
+++ src/main/include/subsystems/SwerveModule.h
@@ -0,0 +1,71 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <numbers>
+
+#include <frc/Encoder.h>
+#include <frc/controller/PIDController.h>
+#include <frc/controller/ProfiledPIDController.h>
+#include <frc/geometry/Rotation2d.h>
+#include <frc/kinematics/SwerveModulePosition.h>
+#include <frc/kinematics/SwerveModuleState.h>
+#include <frc/motorcontrol/Spark.h>
+#include <frc/trajectory/TrapezoidProfile.h>
+#include "ctre/Phoenix.h"
+#include "rev/CANSparkMax.h"
+#include "rev/CANEncoder.h"
+#include <frc/smartdashboard/SmartDashboard.h>
+
+
+
+#include "Constants.h"
+
+class SwerveModule {
+ public:
+    SwerveModule(int m_MotorController, rev::SparkMaxRelativeEncoder::Type m_EncoderType, int m_counts_per_rev, 
+      int m_MotorControllerTurning, 
+      bool driveEncoderReversed,
+      int TurningEncoderNumber,
+      bool turningEncoderReversed
+ );
+
+  frc::SwerveModuleState GetState();
+
+  frc::SwerveModulePosition GetPosition();
+
+  void SetDesiredState(const frc::SwerveModuleState& state);
+
+  void ResetEncoders();
+
+ private:
+  // We have to use meters here instead of radians due to the fact that
+  // ProfiledPIDController's constraints only take in meters per second and
+  // meters per second squared.
+
+  static constexpr auto kModuleMaxAngularVelocity =
+      units::radians_per_second_t{std::numbers::pi * 8.0};
+  static constexpr auto kModuleMaxAngularAcceleration =
+      units::radians_per_second_squared_t{std::numbers::pi * 16.0};
+
+  rev::CANSparkMax* m_driveMotor;
+  rev::CANSparkMax* m_turningMotor;
+
+  rev::SparkMaxRelativeEncoder* m_driveEncoder;
+  ctre::phoenix::sensors::CANCoder* m_turningEncoder;
+  rev::SparkMaxRelativeEncoder::Type m_EncoderType;
+  int m_counts_per_rev;
+
+  bool m_reverseDriveEncoder;
+  bool m_reverseTurningEncoder;
+
+  frc2::PIDController m_drivePIDController{
+      ModuleConstants::kPModuleDriveController, 0, 0};
+  frc::ProfiledPIDController<units::radians> m_turningPIDController{
+      ModuleConstants::kPModuleTurningController,
+      0.0,
+      0.0,
+      {kModuleMaxAngularVelocity, kModuleMaxAngularAcceleration}};
+};
