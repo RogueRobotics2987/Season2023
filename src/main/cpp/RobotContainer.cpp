2b7f770cc718f9f921046bc2b6773f9ca28fd7d3
= <=>
2023-01-28 11:03:29 -0600

    Commanted out GetPathCommand for merge purposes

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -161,32 +161,35 @@ frc2::Command* RobotContainer::GetAutonomousCommand() {
     m_drive.ResetOdometry(frc::Pose2d{5_m, 5_m, 0_deg});
   }
 
-  frc2::Command* RobotContainer::GetPathCommand(){
-    // This will load the file "Example Path.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
-    PathPlannerTrajectory examplePath = PathPlanner::loadPath("New Path", PathConstraints(1.5_mps, 0.5_mps_sq));
 
-   // This will load the file "FullAuto.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
-  // for every path in the group
-  std::vector<PathPlannerTrajectory> pathGroup = PathPlanner::loadPathGroup("FullAuto", {PathConstraints(4_mps, 3_mps_sq)});
+    //Commented out for merge purposes 
 
-  // This is just an example event map. It would be better to have a constant, global event map
-  // in your code that will be used by all path following commands/autobuilders.
-  std::unordered_map<std::string, std::shared_ptr<frc2::Command>> eventMap;
-  eventMap.emplace("marker1", std::make_shared<frc2::PrintCommand>("Passed Marker 1"));
-  eventMap.emplace("intakeDown", std::make_shared<IntakeDown>());
+  // frc2::Command* RobotContainer::GetPathCommand(){
+  //   // This will load the file "Example Path.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
+  //   PathPlannerTrajectory examplePath = PathPlanner::loadPath("New Path", PathConstraints(1.5_mps, 0.5_mps_sq));
 
-  // Create the AutoBuilder. This only needs to be created once when robot code starts, not every time you want to create an auto command. A good place to put this could be in RobotContainer along with your subsystems
+  //  // This will load the file "FullAuto.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
+  // // for every path in the group
+  // std::vector<PathPlannerTrajectory> pathGroup = PathPlanner::loadPathGroup("FullAuto", {PathConstraints(4_mps, 3_mps_sq)});
 
-  SwerveAutoBuilder autoBuilder(
-      [this]() { return swerveSubsystem.getPose(); }, // Function to supply current robot pose
-      [this](auto initPose) { swerveSubsystem.resetPose(initPose); }, // Function used to reset odometry at the beginning of auto
-      PIDConstants(5.0, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
-      PIDConstants(0.5, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
-      [this](auto speeds) { swerveSubsystem.driveFieldRelative(speeds); }, // Output function that accepts field relative ChassisSpeeds
-      eventMap, // Our event map
-      { &swerveSubsystem }, // Drive requirements, usually just a single drive subsystem
-      true // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
-  );
+  // // This is just an example event map. It would be better to have a constant, global event map
+  // // in your code that will be used by all path following commands/autobuilders.
+  // std::unordered_map<std::string, std::shared_ptr<frc2::Command>> eventMap;
+  // eventMap.emplace("marker1", std::make_shared<frc2::PrintCommand>("Passed Marker 1"));
+  // // eventMap.emplace("intakeDown", std::make_shared<IntakeDown>());
 
-  CommandPtr fullAuto = autoBuilder.fullAuto(pathGroup);
-    }
+  // // Create the AutoBuilder. This only needs to be created once when robot code starts, not every time you want to create an auto command. A good place to put this could be in RobotContainer along with your subsystems
+
+  // SwerveAutoBuilder autoBuilder(
+  //     [this]() { return swerveSubsystem.getPose(); }, // Function to supply current robot pose
+  //     [this](auto initPose) { swerveSubsystem.resetPose(initPose); }, // Function used to reset odometry at the beginning of auto
+  //     PIDConstants(5.0, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
+  //     PIDConstants(0.5, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
+  //     [this](auto speeds) { swerveSubsystem.driveFieldRelative(speeds); }, // Output function that accepts field relative ChassisSpeeds
+  //     eventMap, // Our event map
+  //     { &swerveSubsystem }, // Drive requirements, usually just a single drive subsystem
+  //     true // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
+  // );
+
+  // CommandPtr fullAuto = autoBuilder.fullAuto(pathGroup);
+    // }
