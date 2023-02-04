abbb8c8fb2e798e42d99e52020b24e89aa2f3932
= <=>
2023-02-04 14:42:37 -0600

    includes limelight code in DriveSub + button

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -93,6 +93,7 @@ void RobotContainer::ConfigureButtonBindings() {
     frc2::JoystickButton(&m_driverController, 7).OnFalse(m_drive.SetDriveSlow(false));
     frc2::JoystickButton(&m_driverController, 1).OnTrue(m_drive.ButtonZeroHeading());
 
+    frc2::JoystickButton(&m_driverController, 2).OnTrue(m_drive.ConfigOdometry());
 }
 
 frc2::Command* RobotContainer::GetAutonomousCommand() {
