2d1bc89dc39d190c3b1dee307cf44d50533b6185
= <=>
2023-03-02 09:35:59 -0600

    Publish branch

--- src/main/cpp/commands/AutoBalance.cpp
+++ src/main/cpp/commands/AutoBalance.cpp
@@ -18,7 +18,7 @@ void AutoBalance::Initialize() {
   backwardsCheck = false;
   m_timer.Reset();
   m_timer.Stop();
-  autoCheck = m_Joystick->GetRawButton(11);
+  // autoCheck = m_Joystick->GetRawButton(11);
 // Called repea  
   // m_angle = 9;
   // frc::SmartDashboard::PutNumber("m_angle value: ", m_angle);
@@ -26,50 +26,39 @@ void AutoBalance::Initialize() {
 
 // Called repeatedly when this Command is scheduled to run
 void AutoBalance::Execute() {
-  frc::SmartDashboard::PutNumber("Auto State", m_state);
-  frc::SmartDashboard::PutNumber("Auto Pitch", m_drive->GetPitch());
-  frc::SmartDashboard::PutNumber("Auto Timer", m_timer.Get().value());
-  if(m_state == 0) {
+  
+  if(m_state == 0) { //Rams charge station
     // m_angle = frc::SmartDashboard::GetNumber("m_angle value: ", m_angle);
-      frc::SmartDashboard::PutNumber("Auto State", m_state);
-      frc::SmartDashboard::PutNumber("Auto Pitch", m_drive->GetPitch());
-      frc::SmartDashboard::PutNumber("Auto Timer", m_timer.Get().value());
       m_drive->Drive(0_mps, 2.0_mps, 0_rad_per_s, false, false);
       if(m_drive->GetPitch() >= 9 || m_drive->GetPitch() <= -9) {
         m_state = 1;
       }
       
     }
-    if(m_state == 1){
-      frc::SmartDashboard::PutNumber("Auto State", m_state);
-      frc::SmartDashboard::PutNumber("Auto Pitch", m_drive->GetPitch());
-      frc::SmartDashboard::PutNumber("Auto Timer", m_timer.Get().value());
+    else if(m_state == 1){ //Slower climb
       m_drive->Drive(0_mps, 0.45_mps, 0_rad_per_s, false, false);
       if(m_drive->GetPitch() <= 4.5 && m_drive->GetPitch() >= -4.5) {
-        m_state = 2;
+        m_state = 3;
       }
       
     }
-    if(m_state == 2) {
-      frc::SmartDashboard::PutNumber("Auto State", m_state);
-      frc::SmartDashboard::PutNumber("Auto Pitch", m_drive->GetPitch());
-      frc::SmartDashboard::PutNumber("Auto Timer", m_timer.Get().value());
-      if(m_drive->GetPitch() <= 9 && backwardsCheck == false) {
-        m_state = 3;
-        m_timer.Start();
-        backwardsCheck = true;
-      }
-      else if(m_drive->GetPitch() < 10 && m_drive->GetPitch() > -10 && m_timer.Get() > 5.0_s) {
-          m_state = 5;
-        }
-        else if(m_timer.Get() <= 5_s && m_drive->GetPitch() < 10 && m_drive->GetPitch() > -10) {
-          m_drive->Drive(0.0_mps, 0_mps, 0_rad_per_s, false, true);
-        }
-        else {
-          m_state = 4;
-          m_timer.Reset();
-          m_timer.Stop();
-        }
+    // if(m_state == 2) { //Timer/stop state
+    //   if(m_drive->GetPitch() <= 9 && backwardsCheck == false) {
+    //     m_state = 3;
+    //     m_timer.Start();
+    //     backwardsCheck = true;
+    //   }
+    //   else if(m_drive->GetPitch() < 10 && m_drive->GetPitch() > -10 && m_timer.Get() > 5.0_s) {
+    //       m_state = 5;
+    //     }
+    //     else if(m_timer.Get() <= 5_s && m_drive->GetPitch() < 10 && m_drive->GetPitch() > -10) {
+    //       m_drive->Drive(0.0_mps, 0_mps, 0_rad_per_s, false, true);
+    //     }
+    //     else {
+    //       m_state = 4;
+    //       m_timer.Reset();
+    //       m_timer.Stop();
+    //     }
       // else if(m_drive->GetPitch() > 1 || m_drive->GetPitch() < -1) {
       //   m_state = 4;
       //   m_timer.Stop();
@@ -78,37 +67,34 @@ void AutoBalance::Execute() {
       //   m_state = 5;
       // }
       
-    }
-      if(m_state == 3) {
-        frc::SmartDashboard::PutNumber("Auto State", m_state);
-        frc::SmartDashboard::PutNumber("Auto Pitch", m_drive->GetPitch());
-        frc::SmartDashboard::PutNumber("Auto Timer", m_timer.Get().value());
+    // }
+      else if(m_state == 3) { //Run backwards
         if(m_timer.Get() <= 0.75_s) {
           m_drive->Drive(0_mps, -2.0_mps, 0_rad_per_s, false, false);
         }
         else {
-          m_state = 4;
-          m_timer.Reset();
-          m_timer.Stop();
+          m_state = 5;
+          // m_timer.Reset();
+          // m_timer.Stop();
         }
       // if(m_drive->GetPitch() <= 7.75) {
       //   m_state = 2;
       // }
     }
-      if(m_state == 4) {
-        frc::SmartDashboard::PutNumber("Auto State", m_state);
-        frc::SmartDashboard::PutNumber("Auto Pitch", m_drive->GetPitch());
-        frc::SmartDashboard::PutNumber("Auto Timer", m_timer.Get().value());
-        if(m_drive->GetPitch() < 10 && m_drive->GetPitch() > -10) {
-          m_state = 2;
-          m_timer.Start();
-        }
-        else if(m_drive->GetPitch() >= -10) {
-          m_drive->Drive(0_mps, -0.35_mps, 0_rad_per_s, false, false);
-        }
-        else if(m_drive->GetPitch() <= 10) {
-          m_drive->Drive(0_mps, 0.5_mps, 0_rad_per_s, false, false);
-        }
+      // if(m_state == 4) { //Auto Balance state
+      //   if(m_drive->GetPitch() < 10 && m_drive->GetPitch() > -10) {
+      //     m_state = 2;
+      //     m_timer.Start();
+      //   }
+      //   else if(m_drive->GetPitch() <= -10) {
+      //     m_drive->Drive(0_mps, -0.35_mps, 0_rad_per_s, false, false);
+      //   }
+      //   else if(m_drive->GetPitch() >= 10) {
+      //     m_drive->Drive(0_mps, 0.35_mps, 0_rad_per_s, false, false);
+      //   }
+      // }
+      if(m_state == 5){ //Exit state
+        m_drive->Drive(0_mps, 0.0_mps, 0_rad_per_s, false, false);
       }
       // while(m_state == 5) {
       //   frc::SmartDashboard::PutNumber("Auto State", m_state);
@@ -133,9 +119,9 @@ void AutoBalance::Execute() {
 // } else {
 //   m_drive->Drive(0.0_mps, 0_mps, 0_rad_per_s, false, false);
 // }
-if(!m_Joystick->GetRawButton(11) && autoCheck){
-  m_state = 5;
-}
+// if(!m_Joystick->GetRawButton(11) && autoCheck){
+//   m_state = 5;
+// }
 }
     //while(m_state == 2) {
       // m_drive->Drive(0.0_mps, 0_mps, 0_rad_per_s, false, false);
