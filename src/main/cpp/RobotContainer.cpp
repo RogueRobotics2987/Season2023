181ab9d2e77a3ded7536947bc573ca9426fecb87
= <=>
2023-03-02 10:51:11 -0600

    change button for configOdometry

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -151,7 +151,7 @@ void RobotContainer::ConfigureButtonBindings() {
   frc2::JoystickButton(&m_xbox, 3).OnTrue(m_lights.RedColor());
   frc2::JoystickButton(&m_xbox, 4).OnTrue(m_lights.BlueColor());
 
-  frc2::JoystickButton(&m_xbox, 7).OnTrue(m_drive.ConfigOdometry());
+  frc2::JoystickButton(&m_newXbox, 6).OnTrue(m_drive.ConfigOdometry());
 }
 
 
