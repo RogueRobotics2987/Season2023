1be1b1502b721b8eb16e5eedf8d9a9dc9ac6fbef
=KadenKreisler <=kaden.kreisler@outlook.com>
2023-04-11 20:42:16 -0500

    initial Commit

--- src/main/include/commands/InPlaceRotationCmd.h
+++ src/main/include/commands/InPlaceRotationCmd.h
@@ -1,38 +1,38 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// #pragma once
+#pragma once
 
-// #include <frc2/command/CommandBase.h>
-// #include <frc2/command/CommandHelper.h>
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
 
-// #include "subsystems/DriveSubsystem.h"
+#include "subsystems/DriveSubsystem.h"
 
-// /**
-//  * An example command.
-//  *
-//  * <p>Note that this extends CommandHelper, rather extending CommandBase
-//  * directly; this is crucially important, or else the decorator functions in
-//  * Command will *not* work!
-//  */
-// class InPlaceRotationCmd
-//     : public frc2::CommandHelper<frc2::CommandBase, InPlaceRotationCmd> {
-//  public:
-//   InPlaceRotationCmd(double angle);
+/**
+ * An example command.
+ *
+ * <p>Note that this extends CommandHelper, rather extending CommandBase
+ * directly; this is crucially important, or else the decorator functions in
+ * Command will *not* work!
+ */
+class InPlaceRotationCmd
+    : public frc2::CommandHelper<frc2::CommandBase, InPlaceRotationCmd> {
+ public:
+  InPlaceRotationCmd(double angle);
 
-//   void Initialize() override;
+  void Initialize() override;
 
-//   void Execute() override;
+  void Execute() override;
 
-//   void End(bool interrupted) override;
+  void End(bool interrupted) override;
 
-//   bool IsFinished() override;
+  bool IsFinished() override;
 
-//   double m_angle;
+  double m_angle;
 
-//  private:
-//   DriveSubsystem m_drive;
+ private:
+  DriveSubsystem m_drive;
  
 
-// };
+};
